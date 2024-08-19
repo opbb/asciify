809bfe6d6652c178327791899ac7faac8f1ec9b2
Oliver Baer-Benson <opbb83@gmail.com>
2024-08-19 13:05:25 -0600

    Cleaned up user input handling

--- image-to-text.py
+++ image-to-text.py
@@ -2,6 +2,7 @@
 from PIL import Image, ImageFilter
 from math import floor
 from sys import argv
+from os.path import normpath
 
 # DEFAULT VALUES
 reductionAmount = 10
@@ -13,89 +14,67 @@ inputFile = None
 outputFileName = "imageText"
 
 # MESSAGES
-helpMsg = """
-=== IMAGE TO TEXT ===
+helpMsg = """=== IMAGE TO TEXT ===
 image-to-text [OPTIONS]... IN_FILE [OUT_FILE]
 
 OPTIONS:
 -a, --all-chars                         Use all characters to represent an image. Increases quality, but makes all images a similar brightness.
 -r [int], --reduction-amount [int]      Set reduction factor.
 -e, --enhance-edges                     Enhance edges.
--h, --help                              Help.
-"""
+-h, --help                              Help."""
 indexOutOfBoundsErrMsg = "ERROR: Argument index {index} is out of bounds."
 invalidArgumentErrMsg = "ERROR: {argument} is not a valid argument."
-needReductionAmountErrMsg = "ERROR: Must provide a reduction amount with {tag}."
 invalidIntegerArgErrMsg = "ERROR: {argument} is not a valid integer."
 
 # PROCESS COMMAND LINE ARGS
-# there has to be a better way to do this this is so uglY
-# (but im not figureing it out now, I slep)
-def processArgv(i: int) -> None:
-        if (i >= len(argv)):
-            print(indexOutOfBoundsErrMsg.format(index = str(i)))
-            exit(1)
-
-        arg = argv[i]
-        global reductionAmount
-        global useAllCharacters
-        global enhanceEdges
-        global inputFile
-        global outputFileName
-        nextIndex = i + 1
-        if (i == len(argv) - 1):
-            if (inputFile == None):
-                inputFile = arg
-            else:
-                outputFileName = arg
-
-        elif (i == len(argv) - 2 and arg[:1] != "-"):
-            inputFile = arg
-
-        elif (len(arg) < 2):
-            print(invalidArgumentErrMsg.format(argument = arg))
-            exit(1)
-
-        elif (arg[:1] == "-"):
-            if (arg == "-a" or arg == "--all-chars"):
-                useAllCharacters = True
-
-            elif (arg == "-e" or arg == "--enhance-edges"):
-                enhanceEdges = True
-
-            elif (arg == "-h" or arg == "--help"):
-                print(helpMsg)
-                exit(0)
-
-            elif (arg == "-r" or arg == "--reduction-amount"):
-                if (len(argv) - i <= 2):
-                    print(needReductionAmountErrMsg.format(tag = arg))
-                    exit(1)
-
-                if (not argv[i + 1].isdigit()):
-                    print(invalidIntegerArgErrMsg.format(argument = argv[i + 1]))
-                    exit(1)
-                    
-                reductionAmount = int(argv[i + 1])
-                nextIndex += 1
-        
-        else:
-            print(invalidArgumentErrMsg.format(argument = arg))
-            exit(1)
-        
-        if (nextIndex < len(argv)):
-            processArgv(nextIndex)
-        
-        return
-
 if (len(argv) == 1):
     print(helpMsg)
     exit(0)
 
-processArgv(1)
+skipCounter = 0 # used to skip values for options, such as reduction amount
+for i in range(1,len(argv)):
+    arg = argv[i]
 
-if (inputFile == None):
-    print("input file is null")
+    if (skipCounter > 0):
+        skipCounter -= 1
+        continue
+
+    # If this is the last argument, assume it's the input or output file.
+    if (i == len(argv) - 1):
+        if (inputFile == None):
+            inputFile = normpath(arg)
+        else:
+            outputFileName = normpath(arg)
+        continue
+
+    if (arg == "-a" or arg == "--all-chars"):
+        useAllCharacters = True
+        continue
+
+    if (arg == "-e" or arg == "--enhance-edges"):
+        enhanceEdges = True
+        continue
+
+    if (arg == "-h" or arg == "--help"):
+        print(helpMsg)
+        exit(0)
+
+    if (arg == "-r" or arg == "--reduction-amount"):
+        if (not argv[i + 1].isdigit()):
+            print(invalidIntegerArgErrMsg.format(argument = argv[i + 1]))
+            exit(1)
+            
+        reductionAmount = int(argv[i + 1])
+        skipCounter += 1
+        continue
+    
+    # If it's the 2nd to last argument and it is not a valid option, assume it is the input file.
+    if (i == len(argv) - 2):
+        inputFile = normpath(arg)
+        continue
+
+    # If it's not any of the above, it's an invalid argument.
+    print(invalidArgumentErrMsg.format(argument = arg))
     exit(1)
 
 valueToChar = {
