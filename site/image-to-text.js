2aee25e026ba01f841464be42361ade8f08171e9
Oliver Baer-Benson <61638242+opbb@users.noreply.github.com>
2025-04-08 16:05:43 -0500

    testing ffmpeg.wasm on github pages

---
+++ site/image-to-text.js
@@ -0,0 +1,112 @@
+// onmessage = (event) => {
+//   let imageToTextParams = event.data;
+
+//   // Default parameters
+//   let invert = true;
+//   let reductionFactor = 10.0;
+//   let adjustValueRange = true;
+
+//   if (imageToTextParams.invert !== undefined) {
+//     invert = imageToTextParams.invert;
+//   }
+//   if (imageToTextParams.reductionFactor !== undefined) {
+//     reductionFactor = imageToTextParams.reductionFactor;
+//   }
+//   if (imageToTextParams.adjustValueRange !== undefined) {
+//     adjustValueRange = imageToTextParams.adjustValueRange;
+//   }
+
+//   const textImages = imageToText(
+//     imageToTextParams.image,
+//     invert,
+//     reductionFactor,
+//     adjustValueRange,
+//   );
+
+//   postMessage(textImages);
+// };
+
+const valueToPlainText = {
+  0: "\u00A0", // Non-breaking space
+  1: ".",
+  2: ",",
+  3: "-",
+  4: "~",
+  5: ":",
+  6: ";",
+  7: "=",
+  8: "!",
+  9: "*",
+  10: "#",
+  11: "$",
+  12: "@",
+};
+
+valueToHTML = {
+  0: "&nbsp;", // Non-breaking space
+  1: ".",
+  2: ",",
+  3: "-",
+  4: "~",
+  5: ":",
+  6: ";",
+  7: "=",
+  8: "!",
+  9: "*",
+  10: "#",
+  11: "$",
+  12: "@",
+};
+
+function imageToText(
+  image, // Assumes image is Image class from image-js (https://github.com/image-js/image-js)
+  invert = true,
+  reductionFactor = 10.0,
+  adjustValueRange = true,
+) {
+  if (image === undefined || image === null) {
+    return;
+  }
+
+  let reducedGrayscaledImage = image
+    .resize({ factor: 1.0 / reductionFactor })
+    .grey();
+
+  // Default max and min values
+  let maxValue = 255.0;
+  let minValue = 0.0;
+
+  // Adjust max and min values to most extreme values in the image
+  if (adjustValueRange) {
+    maxValue = reducedGrayscaledImage.max[0];
+    minValue = reducedGrayscaledImage.min[0];
+  }
+
+  const rowsArray = [];
+  for (let i = 0; i < reducedGrayscaledImage.height; i++) {
+    rowsArray.push(reducedGrayscaledImage.getRow(i));
+  }
+
+  plainTextImage = "";
+  htmlImage = "";
+  rowsArray.forEach((row) => {
+    row.forEach((pixelValue) => {
+      // Reduces all 256 possible values down to 13, which we have characters to represent
+      charIndex = Math.floor(
+        ((pixelValue - minValue) * 13.0) / (maxValue - minValue + 1.0),
+      );
+      if (invert) {
+        charIndex = 12 - charIndex;
+      }
+      plainTextImage += valueToPlainText[charIndex];
+      htmlImage += valueToHTML[charIndex];
+    });
+    plainTextImage += "\n";
+    htmlImage += "<br/>\n";
+  });
+
+  return {
+    plainTextImage: plainTextImage,
+    htmlImage: htmlImage,
+  };
+}
